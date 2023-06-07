7c0a8cb0cc8732d7401052e9601e80ccebe94951
Tomas Arras <52612929+tomasarras@users.noreply.github.com>
2023-06-06 21:45:48 -0300

    feat(frontend): import discharges

--- frontend/src/pages/imports.jsx
+++ frontend/src/pages/imports.jsx
@@ -1,20 +1,19 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useEffect, useState} from "react";
 import ViewSlider from 'react-view-slider'
-import { Context } from "../context/Context";
 import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
-import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import PaidIcon from '@mui/icons-material/Paid';
 import SchoolIcon from '@mui/icons-material/School';
 import ImportStudents from "../components/section/imports/students";
 import ImportSubscriptionClasses from "../components/section/imports/subscriptionClasses";
+import ImportDischarges from "../components/section/imports/discharges";
 
 export default function Imports() {
     const [activeView, setActiveView] = useState(0);
     const [activeImport, setActiveImport] = useState("");
 
-    const ImportItem = ({ icon, children, item }) => (
-    <div onClick={() => setActiveImport(item)}>
-        <span className={`justify-center cursor-pointer bg-orange-50 text-yellow-900 hover:bg-orange-100 shadow-lg flex items-center rounded-xl font-bold text-sm py-4 px-4`}>
+    const ImportItem = ({ icon, children, item, disabled = false }) => (
+    <div onClick={() => !disabled && setActiveImport(item)}>
+        <span className={`justify-center ${disabled ? "bg-gray-200" : "bg-orange-50 hover:bg-orange-100 cursor-pointer"} text-yellow-900 shadow-lg flex items-center rounded-xl font-bold text-sm py-4 px-4`}>
             {icon}<span className="ml-3">{children}</span>
         </span>
     </div>);
@@ -34,6 +33,7 @@ export default function Imports() {
             (<>
             {activeImport === "students" && <ImportStudents onCancel={onCancelImport}/>}
             {activeImport === "subscriptionClasses" && <ImportSubscriptionClasses onCancel={onCancelImport}/>}
+            {activeImport === "discharges" && <ImportDischarges onCancel={onCancelImport}/>}
             </>)
         }</>
     )
@@ -43,8 +43,8 @@ export default function Imports() {
         <div className="grid gap-4 grid-cols-1 sm:grid-cols-2">
             <ImportItem item="students" icon={<SchoolIcon/>}>Alumnos</ImportItem>
             <ImportItem item="subscriptionClasses" icon={<PaidIcon/>}>Abono de clases</ImportItem>
-            <ImportItem item="headquarters" icon={<AccountBalanceIcon/>}>Sedes</ImportItem>
-            <ImportItem item="courses" icon={<LocalLibraryIcon/>}>Cursos</ImportItem>
+            <ImportItem item="discharges" icon={<PaidIcon/>}>Gastos</ImportItem>
+            <ImportItem item="courses" disabled icon={<LocalLibraryIcon/>}>Cursos</ImportItem>
         </div>
     </>);
     
