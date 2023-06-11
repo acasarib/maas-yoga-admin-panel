affd99af07992983bef6b9ff6977b2a20b774982
Agus Casari <agustincasari@veoestudio.com>
2023-06-11 15:14:07 -0300

    fixing modal margin

--- frontend/src/components/modal.jsx
+++ frontend/src/components/modal.jsx
@@ -25,9 +25,9 @@ export default function Modal(props) {
       <div as="div" className="relative z-10" initialfocus={cancelButtonRef} onClose={onClose}>
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
 
-        <div className="fixed inset-0 overflow-y-auto overflow-x-auto scale-up-center">
+        <div className="fixed inset-0 overflow-y-auto overflow-x-auto">
           <div className="flex min-h-full items-end justify-center p-4 text-center items-center sm:p-0">
-              <div className={`relative transform rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-lg ${getModalSize(props.size)} ${props.className}`} style={props.style}>
+              <div className={`scale-up-center md:ml-64 relative transform rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-lg ${getModalSize(props.size)} ${props.className}`} style={props.style}>
                 <div className="rounded-t-md bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                   <div className="flex flex-col">
                     <div className="modal-header w-full flex justify-between">
