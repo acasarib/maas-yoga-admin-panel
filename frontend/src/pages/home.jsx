e54c8d9bc0448d60cc9413857d24d0be9ae9ca6f
Agus Casari <agustincasari@veoestudio.com>
2023-06-21 20:40:44 -0300

    addding margin

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -87,8 +87,8 @@ export default function Home(props) {
         <>
             <div>
                 <div className="relative bg-orange-50 h-screen overflow-y-auto max-h-screen">
-                <header className="fixed right-0 top-0 z-10  md:left-60 bg-orange-100 py-3 px-4 h-24">
-                    <div className="max-w-4xl mx-auto mt-4">
+                <header className="fixed right-0 top-0 z-10 md:left-60 bg-orange-100 py-3 px-4 h-24">
+                    <div className="max-w-4xl mx-auto mt-4 md:mt-2">
                         <div className="flex items-center justify-between">
                             <div className="md:hidden my-auto">
                                 <HamburgerButton onClick={openSidebar}/>
