16666fc9a7003dc6e1f61c114c51b6cb164de4af
Tomas Arras <52612929+tomasarras@users.noreply.github.com>
2023-08-25 18:48:41 -0300

    feat(frontend): logs table in movements

---
+++ frontend/src/components/section/payments/logsPaymentSection.jsx
@@ -0,0 +1,65 @@
+import React, { useContext, useEffect, useState, useMemo } from "react";
+import { Context } from "../../../context/Context";
+import "react-datepicker/dist/react-datepicker.css";
+import { dateToString } from "../../../utils";
+import Table from "../../table";
+import { LOG_PAYMENT_ACTIONS } from "../../../constants";
+
+export default function LogsPaymentSection(props) {
+    const { getLogs } = useContext(Context);
+    const [logs, setLogs] = useState([]);
+
+    useEffect(async () => {
+        const l = await getLogs();
+        setLogs(l);
+    }, []);
+
+    const getPrettyLogAction = (action, paymentId) => {
+        switch (action) {
+            case LOG_PAYMENT_ACTIONS.CREATE:
+                return `Creacion del pago #${paymentId}`;
+            case LOG_PAYMENT_ACTIONS.UPDATE:
+                return `Se modifico el pago #${paymentId}`;
+            case LOG_PAYMENT_ACTIONS.VERIFICATION:
+                return `Se verifico el pago #${paymentId}`;
+            default:
+                return 'Error';
+        }
+    }
+    
+    const columns = useMemo(() => {
+        const newColumns = [
+            {
+                name: 'Fecha',
+                selector: row => dateToString(row.createdAt),
+                sortable: true,
+                searchable: true,
+            },
+            {
+                name: 'Accion',
+                cell: row => getPrettyLogAction(row.action, row.paymentId),
+                sortable: true,
+            },
+            {
+                name: 'Usuario',
+                cell: row => row?.user?.firstName + " " + row?.user?.lastName,
+                sortable: true,
+                searchable: true,
+            },
+        ];
+        return newColumns;
+    }, []); 
+
+    return (
+        <>
+        <div className="mb-6 md:my-6 md:mx-4">
+            <Table
+                className={`rounded-3xl shadow-lg`}
+                columns={columns}
+                data={logs}
+                pagination paginationRowsPerPageOptions={[5, 10, 25, 50, 100]}
+            />
+        </div>
+        </>
+    );
+} 
