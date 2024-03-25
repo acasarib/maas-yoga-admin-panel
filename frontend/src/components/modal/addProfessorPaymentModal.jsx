1cc0137d1917b78b73e378f390524beec61ecfc6
Tomas Arras <52612929+tomasarras@users.noreply.github.com>
2024-03-24 23:29:12 -0300

    chore: allow add more than 1 professor payment

--- frontend/src/components/modal/addProfessorPaymentModal.jsx
+++ frontend/src/components/modal/addProfessorPaymentModal.jsx
@@ -34,7 +34,6 @@ export default function AddProfessorPaymentModal({ criteriaType, criteriaValue, 
     }
 
     useEffect(() => {
-        console.log(criteriaType, "criteriaType");
         if (amountStudents == '') {
             setError(false)
             return
