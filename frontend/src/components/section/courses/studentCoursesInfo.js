ba9949aa951fe660049c53c56adffd29b8cf3b81
Tomas Arras <52612929+tomasarras@users.noreply.github.com>
2024-04-11 23:48:54 -0300

    chore: suspend students information

--- frontend/src/components/section/courses/studentCoursesInfo.js
+++ frontend/src/components/section/courses/studentCoursesInfo.js
@@ -4,6 +4,9 @@ import StudentCalendar from '../../calendar/studentCalendar'
 import DangerousIcon from '@mui/icons-material/Dangerous';
 import Tooltip from '@mui/material/Tooltip';
 import CheckIcon from '@mui/icons-material/Check';
+import { STUDENT_MONTHS_CONDITIONS } from '../../../constants';
+import ErrorIcon from '@mui/icons-material/Error';
+import PaidIcon from '@mui/icons-material/Paid';
 
 const StudentCoursesInfo = ({ student }) => {
 
@@ -13,6 +16,7 @@ const StudentCoursesInfo = ({ student }) => {
 		<Modal
 		hiddingButton
 		open={isOpen}
+		icon={<PaidIcon/>}
 		size="large"
 		setDisplay={() => setIsOpen(false)}
 		buttonText={"Aplicar"}
@@ -22,8 +26,9 @@ const StudentCoursesInfo = ({ student }) => {
 		</Modal>
 		<div className='flex'>
 			<div className='underline text-yellow-900 mx-1 cursor-pointer' onClick={() => setIsOpen(true)}>Ver pagos</div>
-			{student.currentMonth == 'NOT_PAID' && <Tooltip title="Mes actual impago"><DangerousIcon style={{ color: '#FF676D' }}/></Tooltip>}
-			{student.currentMonth == 'PAID' && <Tooltip title="Al dia"><CheckIcon style={{ color: '#72EA8D' }}/></Tooltip>}
+			{student.currentMonth == STUDENT_MONTHS_CONDITIONS.NOT_PAID && <Tooltip title="Mes actual impago"><DangerousIcon style={{ color: '#FF676D' }}/></Tooltip>}
+			{student.currentMonth == STUDENT_MONTHS_CONDITIONS.PAID && <Tooltip title="Al dia"><CheckIcon style={{ color: '#72EA8D' }}/></Tooltip>}
+			{student.currentMonth == STUDENT_MONTHS_CONDITIONS.SUSPEND && <Tooltip title="Suspendido"><ErrorIcon style={{ color: '#FFCD30' }}/></Tooltip>}
 		</div>
 		</>)
 }
