ee2e410cb61efb59b3bffbfc375b72ffd5c9f556
Agustin Casari Bacas <agustincasaribacas@hotmail.com>
2024-04-13 13:27:21 -0300

    needsRegistration fcheck added

--- frontend/src/components/checkbox/customCheckbox.jsx
+++ frontend/src/components/checkbox/customCheckbox.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Checkbox from '@mui/material/Checkbox';
+import { orange } from '@mui/material/colors';
 
 export default function CustomCheckbox({ checked, labelOn, labelOff, className = "", onChange = () => {}, disabled = false }) {
 
     return(
         <>
             <div className={`${className}`}>
-                <Checkbox onChange={onChange} checked={checked} disabled={disabled} />
+                <Checkbox onChange={onChange} checked={checked} disabled={disabled} sx={{
+                      color: orange[500],
+                      '&.Mui-checked': {
+                        color: orange[500],
+                      },
+                    }} name="checkbox"/>
                 {checked ? labelOn : labelOff}
             </div>
         </>
