255373c668d39442a32bee8d028a030356eb642e
Tomas Arras <52612929+tomasarras@users.noreply.github.com>
2024-07-20 16:49:19 -0300

    fix: filter by date in payments table

--- frontend/src/components/paymentsTable/index.jsx
+++ frontend/src/components/paymentsTable/index.jsx
@@ -13,7 +13,7 @@ import VerifyPaymentModal from "../modal/verifyPaymentModal";
 import useModal from "../../hooks/useModal";
 import DeletePaymentModal from "../modal/deletePaymentModal";
 
-export default function PaymentsTable({ columnsProps = [],dateField = "at", className = "", payments, defaultSearchValue, defaultTypeValue, isLoading, canVerify, editPayment, editMode, onClickDeletePayment, onClickVerifyPayment }) {
+export default function PaymentsTable({ columnsProps = [], dateField = "at", className = "", payments, defaultSearchValue, defaultTypeValue, isLoading, canVerify, editPayment, editMode, onClickDeletePayment, onClickVerifyPayment }) {
     const { user, categories, changeAlertStatusAndMessage, getCourseById, getUserById } = useContext(Context);
     const [payment, setPayment] = useState(null);
     const verifyPaymentModal = useModal()
@@ -190,7 +190,7 @@ export default function PaymentsTable({ columnsProps = [],dateField = "at", clas
             },
             {
                 name: 'Fecha',
-                selector: row => row[dateField],
+                selector: row => dateToString(row[dateField]),
                 cell: row => <span>{dateToString(row[dateField])}</span>,
                 sortable: true,
                 searchable: true,
@@ -293,7 +293,7 @@ export default function PaymentsTable({ columnsProps = [],dateField = "at", clas
             }
         })
         return columns;
-    }, [categories, dateField, columnsProps]); 
+    }, [categories, dateField]); 
 
     useEffect(() => {
         setFilteredPayments(payments);
