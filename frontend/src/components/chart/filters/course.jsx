28c0298d75dd2c3760f3fd334c4879c188e3f9c8
Agustin Casari Bacas <agustincasaribacas@hotmail.com>
2025-03-25 19:38:28 -0300

    all courses check

--- frontend/src/components/chart/filters/course.jsx
+++ frontend/src/components/chart/filters/course.jsx
@@ -1,23 +1,35 @@
 import React, { useEffect, useState } from "react";
 import SelectCourses from "../../select/selectCourses";
+import CustomCheckbox from "../../checkbox/customCheckbox";
 
 export default function FilterPaymentCourse({ onChange }) {
 
     const [selectedCourse, setSelectedCourse] = useState(null);
+    const [allCourses, setAllCourses] = useState(false);
     
     useEffect(() => {
-        if (selectedCourse !== null)
+        if (selectedCourse !== null) {
             onChange(`courseId eq ${selectedCourse.id}`);
-    }, [selectedCourse]);
+        }else {
+            if(allCourses) onChange(`courseId ne null`);
+        }
+    }, [selectedCourse, allCourses]);
     
     return (
     <div>
         <span className="block text-gray-700 text-sm font-bold mb-2">Curso</span>
-        <div className="flex">
+        <div className="flex space-x-4 items-end">
             <SelectCourses
                 className="payment-filter-width"
                 value={selectedCourse}
                 onChange={setSelectedCourse}
+            />
+            <CustomCheckbox
+                checked={allCourses}
+                labelOn="Todos"
+                labelOff="Todos"
+                className="ml-2"
+                onChange={() => setAllCourses(!allCourses)}
             />
         </div>
     </div>
