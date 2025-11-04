d0aa5fe3dcdf7858d2d7ff10a87db1bbaf2a4431
Tomas Arras <52612929+tomasarras@users.noreply.github.com>
2025-11-03 22:06:46 -0300

    styles: spacing changes

--- frontend/src/components/commonInput.jsx
+++ frontend/src/components/commonInput.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import CommonTextArea from "./commonTextArea";
+import Label from "./label/label";
 
 export default function CommonInput(props) {
 
@@ -9,30 +11,32 @@ export default function CommonInput(props) {
             props.onKeyDown();
     }
 
+    if (props.type === 'textarea')
+        return <div><CommonTextArea {...props}/></div>
+
     return(
-        <>
-            <label className={props.className ? props.className : "block text-gray-700 text-sm font-bold mb-2"} htmlFor={props.htmlFor || props.name}>
+        <div>
+            <Label className={props.className ? props.className : ""} htmlFor={props.htmlFor || props.name}>
                 {props.label}
-            </label>
-            <input className={`${props.inputClassName} shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-[#ff9800] focus:ring-1 focus:ring-[#ff9800]`} 
-                id={props.id || props.name} 
-                type={props.type} 
-                placeholder={props.placeholder} 
-                onChange={props.onChange}
-                onFocus={props.onFocus}
-                onBlur={props.onBlur}
-                value={props.value}
-                autoComplete={props.autoComplete}
-                name={props.name}
-                role={props.role}
-                htmlFor={props.htmlFor}
-                min={props.min}
-                disabled={props.disabled}
-                onKeyDown={handleOnKeyDown}
-            />
+            </Label>
+            {props.currency === true ? (
+                <div className="relative">
+                    <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-500">$</span>
+                    <input {...props} className={`${props.inputClassName} shadow appearance-none border rounded w-full py-2 pl-7 pr-3 text-gray-700 leading-tight focus:outline-none focus:border-[#ff9800] focus:ring-1 focus:ring-[#ff9800]`} 
+                        id={props.id || props.name}
+                        onKeyDown={handleOnKeyDown}
+                    />
+                </div>
+            ) : (
+                <input {...props} className={`${props.inputClassName} shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-[#ff9800] focus:ring-1 focus:ring-[#ff9800]`} 
+                    id={props.id || props.name}
+                    onKeyDown={handleOnKeyDown}
+                />
+            )}
+            
             {props.isInvalid === true && 
             <div className="text-red-600">{props.invalidMessage}</div>
             }
-        </>
+        </div>
     );
 } 
